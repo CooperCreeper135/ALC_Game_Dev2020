2af36f4ce758fe62571bb757b2dfde217fbe8dbc
Cooper <cooper.p673@stu.nebo.edu>
2019-10-09 10:28:52 -0600

    food

--- CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 15;
+    public float speed = 30;
+    public float xRange = 20;
 
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +19,23 @@ public class PlayerController : MonoBehaviour
     void Update()
 
     {
-        if (transform.position.x < -20)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-20, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > 20)
+        if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
 
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
