dda8493be94a0361e22d8cf3500945a87ef1414a
Cooper <cooper.p673@stu.nebo.edu>
2019-10-11 10:27:12 -0600

    spawn

--- CreateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ CreateWithCode/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    public float topBound = 30;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float topBound = 30f;
+    public float lowerBound = -10f;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
