eef1ba66e8250ebac86a6557133e5c08e25a4698
Cooper <cooper.p673@stu.nebo.edu>
2019-10-15 10:29:07 -0600

    epeeec

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
