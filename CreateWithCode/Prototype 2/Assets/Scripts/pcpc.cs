2cca97aced7ffee7d3d72667607cc2636bffc2a2
Cooper <cooper.p673@stu.nebo.edu>
2019-10-25 10:30:12 -0600

    head simulator

---
+++ CreateWithCode/Prototype 2/Assets/Scripts/pcpc.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pcpc : MonoBehaviour
+{
+    private float speed = 0.25f;
+    private float rotationSpeed = 200.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * horizontalInput);
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * verticalInput);
+    }
+}
+
