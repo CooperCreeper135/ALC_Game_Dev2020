5f848fb3bf0829b6763d031c2aa5c8db73714d1a
Cooper <cooper.p673@stu.nebo.edu>
2019-11-12 10:26:10 -0700

    rocketman

--- CreateWithCode/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,23 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityModifier; 
 
     }
-
+    public bool isOnGround = true;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) { playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); }
-            
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround) { playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); isOnGround = false; }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game OVer!");
+        }
     }
 }
