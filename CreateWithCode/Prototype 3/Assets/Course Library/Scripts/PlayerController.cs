f8c73e6ffdb107cb69ea83407734efe739c6a2dc
Cooper <cooper.p673@stu.nebo.edu>
2019-11-18 10:06:59 -0700

    be my mirror my sword and shield

--- CreateWithCode/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,19 +8,28 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier; 
 
     }
-    public bool isOnGround = true;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround) { playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); isOnGround = false; }
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+
+        }
 
     }
     private void OnCollisionEnter(Collision collision)
@@ -31,8 +40,11 @@ public class PlayerController : MonoBehaviour
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            gameOver = true;
             Debug.Log("Game OVer!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
         }
     }
 }
