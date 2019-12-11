92aa554a5295ddc7ac0c0ac6db6d30ddf3552506
Cooper <cooper.p673@stu.nebo.edu>
2019-12-11 09:23:48 -0700

    ye

---
+++ CreateWithCode/Cooper Parkin - Personal Project/Assets/scripts/down.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class down : MonoBehaviour
+
+{
+    public Rigidbody RB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RB.AddForce(Vector3.down * 10);
+    }
+}
---
+++ CreateWithCode/Cooper Parkin - Personal Project/Assets/scripts/smol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.localScale -= new Vector3(.1f, .1f, .1f);
+        if (transform.localScale.x < .1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
