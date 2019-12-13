b2afe2d80d4fd69ff00252dce28dab073b462651
Cooper <cooper.p673@stu.nebo.edu>
2019-12-13 15:32:10 -0700

    o

--- CreateWithCode/Prototype 4/Assets/Course Library/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 4/Assets/Course Library/Scripts/SpawnManager.cs
@@ -6,16 +6,34 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
 
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(3);
+        
+        
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i =0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        
+        
+        enemyCount = FindObjectOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
         
     }
 
