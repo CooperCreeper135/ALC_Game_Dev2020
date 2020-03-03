0d245c2eaec83b015c9f0353107e88b4347e8572
cooper parkin <cooper.p673@stu.nebo.edu>
2020-03-03 07:58:16 -0700

    problably grass, i dont know.

--- CreateWithCode/Cooper Parkin - Personal Project/Assets/appe.cs
+++ CreateWithCode/Cooper Parkin - Personal Project/Assets/appe.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class appe : MonoBehaviour
 {
-    public GameObject gameObject;
-
+    public new GameObject gameObject;
+    public static bool first = true;
     void Start()
     {
         StartCoroutine(RemoveAfterSeconds(3, gameObject));
     }
     IEnumerator RemoveAfterSeconds(int seconds, GameObject image)
     {
-        yield return new WaitForSeconds(3);
-        image.SetActive(false);
+        if (first == true)
+        {
+            yield return new WaitForSeconds(3);
+            first = false;
+            image.SetActive(false);
+
+
+        }
+        if (first == false)
+        {
+            
+            image.SetActive(false);
+        }
+
+
     }
 
 }
