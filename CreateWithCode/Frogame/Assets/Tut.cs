b8a20a1397c35da309932c6e5424f0a92b9f56fa
cooper parkin <cooper.p673@stu.nebo.edu>
2020-03-15 00:30:34 -0600

    frogayme

--- CreateWithCode/Frogame/Assets/Tut.cs
+++ CreateWithCode/Frogame/Assets/Tut.cs
@@ -8,13 +8,14 @@ public class Tut : MonoBehaviour
 {
     public PlayerMovement player;
     public LLL nerd;
+    public int james;
     void Update()
     {
         if (nerd.open == true)
         {
             if (player.lvlgo == true)
             {
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(james);
                 Debug.Log("what");
             }
         }
